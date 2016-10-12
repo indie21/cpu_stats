8f65829efa6a7ff86473287fb6ddc8082f41b491
zhuoyikang <zhuoyikang@gmail.com>
2016-10-12 22:09:07 +0800

    java 定时器使用

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 import com.timgroup.statsd.StatsDClient;
 import com.timgroup.statsd.NonBlockingStatsDClient;
+import java.util.Timer;
 
 
 /**
@@ -9,10 +10,14 @@ import com.timgroup.statsd.NonBlockingStatsDClient;
  */
 public class App
 {
+    private static Timer timer;
     //private static final StatsDClient statsd = new NonBlockingStatsDClient("my.prefix", "statsd-host", 8125);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        timer = new Timer();
+        timer.schedule(new StatsdTask(), 0, 3 * 1000);
+
         // statsd.incrementCounter("bar");
         // statsd.recordGaugeValue("baz", 100);
         // statsd.recordExecutionTime("bag", 25);
---
+++ src/main/java/com/mycompany/app/StatsdTask.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+
+import java.util.TimerTask;
+
+public class StatsdTask extends TimerTask {
+    public void run() {
+        System.out.println("Time's up!!!!");
+    }
+}
