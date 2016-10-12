990dd18deb521708fd62d38e77628cf65e2fcd37
zhuoyikang <zhuoyikang@gmail.com>
2016-10-12 22:00:21 +0800

    ignore

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,21 @@
 package com.mycompany.app;
+import com.timgroup.statsd.StatsDClient;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    //private static final StatsDClient statsd = new NonBlockingStatsDClient("my.prefix", "statsd-host", 8125);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        // statsd.incrementCounter("bar");
+        // statsd.recordGaugeValue("baz", 100);
+        // statsd.recordExecutionTime("bag", 25);
+        // statsd.recordSetEvent("qux", "one");
     }
 }
