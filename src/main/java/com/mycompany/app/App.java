0feb2f085d65151393a273c75884a1b7526a3e81
zhuoyikang <zhuoyikang@gmail.com>
2016-10-15 10:07:32 +0800

    定时输出cpu使用率到stastd

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
+import com.sun.scenario.animation.shared.TimerReceiver;
 import com.timgroup.statsd.NonBlockingStatsDClient;
+
 import java.util.Timer;
 
 
@@ -9,10 +11,16 @@ public class App
     public static void main( String[] args )
     {
         showLibraryPath();
+        timerRun();
     }
 
     public static void showLibraryPath() {
         String path = System.getProperty("java.library.path");
-        System.out.print("java.library.path is:" + path);
+        System.out.print("java.library.path is:" + path + "\n");
+    }
+
+    public static void timerRun() {
+        timer = new Timer();
+        timer.schedule(new StatsdTask(), 0, 3 * 1000);
     }
 }
---
+++ src/main/java/com/mycompany/app/StatsdTask.java
@@ -0,0 +1,26 @@
+package com.mycompany.app;
+
+import java.util.TimerTask;
+import com.timgroup.statsd.StatsDClient;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class StatsdTask extends TimerTask {
+
+    private StatsDClient statsd;
+
+    public StatsdTask() {
+        statsd = new NonBlockingStatsDClient("cpu", "cando-v3-zhuoyikang.hzv.tenxapp.com", 59058);
+    }
+
+    public void run() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        String time = df.format(new Date());// new Date()为获取当前系统时间
+        double  utild =  CpuStats.utilization();
+        long util = (long)(utild*100);
+        System.out.printf("%s: cpu utilization:%d\n", time, util);
+        statsd.recordGaugeValue("baz", util);
+    }
+}
