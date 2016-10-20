e4909f00188626dd7f1b14271f20bad791a5d906
zhuoyikang <zhuoyikang@gmail.com>
2016-10-20 19:57:25 +0800

    new

--- src/main/java/com/mycompany/app/StatsdTask.java
+++ src/main/java/com/mycompany/app/StatsdTask.java
@@ -12,18 +12,26 @@ public class StatsdTask extends TimerTask {
     private StatsDClient statsd;
 
     public StatsdTask() {
-        statsd = new NonBlockingStatsDClient(Config.config.getIdentity(),
-                                             Config.config.getStastdHost(),
-                                             Config.config.getStastdPort());
+        // statsd = new NonBlockingStatsDClient(Config.config.getIdentity(),
+        //                                      Config.config.getStastdHost(),
+        //                                      Config.config.getStastdPort());
     }
 
     public void run() {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        String time = df.format(new Date());// new Date()为获取当前系统时间
-        double  utild =  CpuStats.utilization();
-        long util = (long)(utild*100);
-        System.out.printf("%s: cpu utilization:%d\n", time, util);
-        statsd.recordGaugeValue("baz", util);
-        statsd.recordGaugeValue("top", 100);
+
+        try {
+            StatsDClient statsd = new NonBlockingStatsDClient(Config.config.getIdentity(),
+                                                              Config.config.getStastdHost(),
+                                                              Config.config.getStastdPort());
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+            String time = df.format(new Date());// new Date()为获取当前系统时间
+            double  utild =  CpuStats.utilization();
+            long util = (long)(utild*100);
+            System.out.printf("%s: cpu utilization:%d\n", time, util);
+            statsd.recordGaugeValue("baz", util);
+            statsd.recordGaugeValue("top", 100);
+        } catch(Exception ex) {
+            System.out.printf("exception\n");
+        }
     }
 }
